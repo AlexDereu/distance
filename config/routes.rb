19817e550de709804731b0126f128621e9e6902d
Alex Dereu <adereu08@gmail.com>
2022-06-17 12:01:10 +0200

    MVP

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get   "villes",          to: "villes#index"
+  get   "villes/new",      to: "villes#new", as: :new_ville
+  post  "villes",          to: "villes#create"
+  get   "villes/:id",      to: "villes#show", as: :ville
+  get   "villes/:id/edit", to: "villes#edit", as: :edit_ville
+  patch "villes/:id",      to: "villes#update"
+  delete "villes/:id",     to: "villes#destroy"
 end
